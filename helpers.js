8f6a96898e5a5a798eb62b22881ed0a824d6cc23
Michael Scoli <mscoli44@gmail.com>
2022-11-11 19:07:42 -0500

    Tweaked helper functions and added static JS file to error check form input

--- helpers.js
+++ helpers.js
@@ -12,30 +12,46 @@ function checkId(arg, argName) {
     return arg;
 }
 
-function checkFirstName(name){
-    if(!name) throw "Error: You must provide a first name."
+function checkStringErrors(arg, argName) {
+    if (typeof arg === 'undefined') throw `Error: The ${argName} argument was not provided`;
+    if (typeof arg !== "string") throw `Error: The ${argName} argument is of the ${typeof arg} type; it should be of the string type`;
+    if (arg === "") throw `Error: The ${argName} argument is an empty string`;
+    if (arg.trim() === '') throw `Error: The ${argName} argument is a string that only contains spaces`;
+
+    arg = arg.trim();
+    return arg;
+}
+
+function checkFirstName(firstName){
+    firstName = checkStringErrors(firstName, 'firstName');
+
+    for (let i = 0; i < firstName.length; i++) {
+        if (!(/^[A-Za-z'-]/g.test(firstName[i]))) throw `The first name must only contain letters a-z or A-Z, an apostrophe, or a hyphen`;
+    }
+
+    return firstName;
 }
 
 function checkEmail(email){
-    if(!email) throw "Error: You must provide an email."
+    if(!email) throw "Error: You must provide an email.";
 }
 function checkLocation(location){
-    if(!location) throw "Error: You must provide a location."
+    if(!location) throw "Error: You must provide a location.";
 }
 function checkBDay(day){
-    if(!day) throw "Error: You must provide a day."
+    if(!day) throw "Error: You must provide a day.";
 }
 function checkBMon(mon){
-    if(!mon) throw "Error: You must provide a month."
+    if(!mon) throw "Error: You must provide a month.";
 }
 function checkBYear(year){
-    if(!year) throw "Error: You must provide a year."
+    if(!year) throw "Error: You must provide a year.";
 }
 function checkGender(gender){
-    if(!gender) throw "Error: You must provide a gender."
+    if(!gender) throw "Error: You must provide a gender.";
 }
 function checkOrientation(orient){
-    if(!orient) throw "Error: You must provide a first name."
+    if(!orient) throw "Error: You must provide a first name.";
 }
 
 module.exports = {checkId, checkFirstName, checkEmail, checkLocation, checkBDay, checkBMon, checkBYear, checkGender, checkOrientation};
